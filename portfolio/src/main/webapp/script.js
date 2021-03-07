50bf913f5c43ee1e15ca7cac7a501d259e790293
abellegesse03 <abellegesse03@gmail.com>
2021-03-07 17:48:43 +0000

    Add button on landing page to fetch content directly from servlet.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,10 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+async function addHelloWorld() {
+  const responseFromServer = await fetch('/hello');
+  const textFromResponse = await responseFromServer.text();
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const helloWorldContainer = document.getElementById('helloworld-container');
+  helloWorldContainer.innerText = textFromResponse;
 }
